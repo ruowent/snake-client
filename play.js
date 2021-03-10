c158374efcece8aa75735119dff316b89d1bb7d3
Ruowen Tang <ruowen.tang@gmail.com>
2021-03-10 14:50:25 -0500

    added handleUserInput function for ctrl c

--- play.js
+++ play.js
@@ -3,3 +3,26 @@ const {connect} = require('./client.js');
 
 console.log('Connecting ...');
 connect();
+//let conn;
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+ const setupInput = function() {
+  //conn = connection;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
